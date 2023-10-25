682ef0d2ab1460e015599bab7fd8dafbbbe035a3
Mrying0823 <1553947217@qq.com>
2023-10-25 23:52:55 +0800

    控制器

---
+++ src/main/java/org/mrying/chat/controller/BaseController.java
@@ -0,0 +1,19 @@
+package org.mrying.chat.controller;
+
+import org.mrying.chat.service.ChatgptService;
+import org.mrying.chat.service.ConversationService;
+
+import javax.annotation.Resource;
+
+/**
+ * @author 邓和颖
+ * @since 2023/10/14 17:36
+ */
+public class BaseController {
+
+    @Resource
+    protected ConversationService conversationService;
+
+    @Resource
+    protected ChatgptService chatgptService;
+}
