5f21413102a31acb1ab96f64ad3fc2d03761df0c
Mrying0823 <1553947217@qq.com>
2023-10-27 17:33:06 +0800

    chatgptMessage 实体类

---
+++ src/main/java/org/mrying/chat/model/ChatgptMessage.java
@@ -0,0 +1,55 @@
+package org.mrying.chat.model;
+
+import java.util.Date;
+
+public class ChatgptMessage {
+    private String messageId;
+
+    private Integer messageDirection;
+
+    private String conversationId;
+
+    private String content;
+
+    private Date createTime;
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+
+    public Integer getMessageDirection() {
+        return messageDirection;
+    }
+
+    public void setMessageDirection(Integer messageDirection) {
+        this.messageDirection = messageDirection;
+    }
+
+    public String getConversationId() {
+        return conversationId;
+    }
+
+    public void setConversationId(String conversationId) {
+        this.conversationId = conversationId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
