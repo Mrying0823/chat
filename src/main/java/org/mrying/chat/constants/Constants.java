59a87156a19532f0f1c33a47a37cfea1da782048
Mrying0823 <1553947217@qq.com>
2023-10-27 17:34:48 +0800

    常量

---
+++ src/main/java/org/mrying/chat/constants/Constants.java
@@ -0,0 +1,15 @@
+package org.mrying.chat.constants;
+
+/**
+ * @author 邓和颖
+ * @since 2023/10/14 19:52
+ */
+public class Constants {
+
+    // 消息发送方向
+    public static final int DIRECTION_QUESTION = 0;
+    public static final int DIRECTION_ANSWER = 1;
+
+    // 发送消息时加入的上下文信息数
+    public static final int MESSAGE_LIMIT_NUM = 6;
+}
