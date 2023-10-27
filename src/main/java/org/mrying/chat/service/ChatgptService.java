2cdb62878f3e88768f90395c7c9f7e2647da5ff9
Mrying0823 <1553947217@qq.com>
2023-10-27 17:29:33 +0800

    根据 conversationId 查询某个会话的若干条对话

--- src/main/java/org/mrying/chat/service/ChatgptService.java
+++ src/main/java/org/mrying/chat/service/ChatgptService.java
@@ -1,6 +1,9 @@
 package org.mrying.chat.service;
 
+import org.mrying.chat.model.ChatgptMessage;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.List;
 
 /**
  * @author 邓和颖
@@ -10,4 +13,7 @@ public interface ChatgptService {
 
     // 向 chatgpt 提问
     SseEmitter sendMsgBySse(Boolean publicApi,String conversationId,String prompt);
+
+    // 根据 conversationId 查询某个会话的若干条对话
+    List<ChatgptMessage> queryMessagesByConversationId(String conversationId,Integer limit);
 }
