c28a10108cee0f20e95121299063c45700d1cc7b
Mrying0823 <1553947217@qq.com>
2023-10-27 10:31:26 +0800

    新增 Post 建立 sse 连接方法

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import GPTWindow from "@/views/GPTWindow";
 import ChatGPT from "@/views/ChatGPT";
+import ChatGPTPost from "@/views/ChatGPTPost";
 import GPTWindow3 from "@/views/GPTWindow3";
 
 const routes = [
@@ -18,6 +19,11 @@ const routes = [
     path: "/gpt/gptWindow3",
     name: "GPTWindow3",
     component: GPTWindow3
+  },
+  {
+    path: "/gpt/chatGPTPost",
+    name: "ChatGPTPost",
+    component: ChatGPTPost
   }
 ]
 
