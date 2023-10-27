562df8b97db856ff5812b4f5d876483d1ff21bb5
Mrying0823 <1553947217@qq.com>
2023-10-27 17:33:53 +0800

    响应类

---
+++ src/main/java/org/mrying/chat/view/RespResult.java
@@ -0,0 +1,106 @@
+package org.mrying.chat.view;
+
+import org.mrying.chat.enums.RespCode;
+
+import java.util.List;
+
+/**
+ * @author 邓和颖
+ * @since 2023/9/24 1:13
+ */
+
+// 统一应答结果类，controller 返回值
+public class RespResult {
+
+    // 应答码
+    private int code;
+
+    // code 的文字说明，用于提示
+    private String msg;
+
+    // 单个数据
+    private Object retData;
+
+    // 集合数据
+    private List list;
+
+    private String accessToken;
+
+    private String conversationId;
+
+    // 表示成功的 RespResult 对象
+    public static RespResult ok() {
+        RespResult respResult = new RespResult();
+        respResult.setCode(RespCode.SUCCESS.getCode());
+        respResult.setMsg(RespCode.SUCCESS.getMsg());
+        return respResult;
+    }
+
+    // 表示失败的 RespResult 对象
+    public static RespResult fail() {
+        RespResult respResult = new RespResult();
+        respResult.setCode(RespCode.BAD_REQUEST.getCode());
+        respResult.setMsg(RespCode.BAD_REQUEST.getMsg());
+        return respResult;
+    }
+
+    public static RespResult notFound() {
+        RespResult respResult = new RespResult();
+        respResult.setCode(RespCode.NOT_FOUND.getCode());
+        respResult.setMsg(RespCode.NOT_FOUND.getMsg());
+        return respResult;
+    }
+
+    public void setRespCode(RespCode respCode) {
+        this.code = respCode.getCode();
+        this.msg = respCode.getMsg();
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getRetData() {
+        return retData;
+    }
+
+    public void setRetData(Object retData) {
+        this.retData = retData;
+    }
+
+    public List getList() {
+        return list;
+    }
+
+    public void setList(List list) {
+        this.list = list;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getConversationId() {
+        return conversationId;
+    }
+
+    public void setConversationId(String conversationId) {
+        this.conversationId = conversationId;
+    }
+}
