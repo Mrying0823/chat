43b08ae7cca23fbcd694a3c3c3eb55a83da300d9
Mrying0823 <1553947217@qq.com>
2023-11-04 00:57:37 +0800

    使用 redis

---
+++ src/main/java/org/mrying/chat/constants/RedisKey.java
@@ -0,0 +1,11 @@
+package org.mrying.chat.constants;
+
+/**
+ * @author 邓和颖
+ * @since 2023/11/3 11:36
+ */
+public class RedisKey {
+
+    // 用户提示
+    public static final String KEY_USER_QUESTION = "USER_QUESTION";
+}
