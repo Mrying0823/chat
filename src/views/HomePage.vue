ac4893a04fcc1672313430ded6855750948123a1
Mrying0823 <1553947217@qq.com>
2023-11-07 00:11:45 +0800

    新增首页走马灯图片

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -12,7 +12,7 @@
                 <el-card shadow="hover" body-style="" class="card">
                   <div class="el-row">
                     <div class="el-col-16">
-                      <img class="card-image"  src="" alt="hi">
+                      <img class="card-image"  src="../assets/img/作业.png" alt="hi">
                     </div>
                     <div class="el-col-8 card-intro">
                       <div class="card-title">
@@ -43,7 +43,7 @@
                         </router-link>
                       </div>
                       <div class="card-text">
-                        提供ChatGPT接口，通过Token或OpenAI Api-Key访问，学习与工作的好帮手
+                        提供 ChatGPT 接口，通过 Token 或 OpenAI Api-Key 访问，学习与工作的好帮手
                       </div>
                     </div>
                   </div>
@@ -53,7 +53,7 @@
                 <el-card shadow="hover" body-style="" class="card">
                   <div class="el-row">
                     <div class="el-col-16">
-                      <img class="card-image"  src="" alt="hi">
+                      <img class="card-image"  src="../assets/img/面试.png" alt="hi">
                     </div>
                     <div class="el-col-8 card-intro">
                       <div class="card-title">
@@ -63,7 +63,7 @@
                         </router-link>
                       </div>
                       <div class="card-text">
-                        速通高频面试题，记录回答音频，ChatGPT辅助解答，完善您的答案
+                        速通高频面试题，记录回答音频，ChatGPT 辅助解答，完善您的答案
                       </div>
                     </div>
                   </div>
@@ -99,69 +99,4 @@ export default {
 </script>
 
 <style scoped>
-.home-container {
-  width: 100%; /* 设置容器宽度为100% */
-  box-sizing: border-box; /* 设置盒模型为border-box，确保内边距和边框不会增加容器的宽度 */
-  height: 100vh;
-  border: 1px solid #ccc;
-  box-shadow: 2px 2px 10px #ccc;
-  font-family: "Source Code Pro Light", sans-serif;
-  position: fixed;
-  background-color: #00C9CD;
-}
-
-#particles-js {
-  position: absolute;
-  height: 100%;
-  width: 100%;
-}
-
-.container {
-  font-family: "Source Code Pro Light",sans-serif;
-  background-color: #00C9CD;
-  background-repeat: no-repeat;
-  padding-left: 5%;
-  padding-right: 8%;
-  box-sizing: border-box;
-}
-
-.left {
-  height: 68vh;
-  margin: 12vh 0 10vh 0;
-}
-
-.intro {
-  padding-left: 10%;
-  margin: auto;
-}
-
-h1 {
-  color: rgba(246, 247, 248, 0.82);
-  font-size: 60px;
-  margin: 0;
-}
-
-h3 {
-  color: rgba(246, 247, 248, 0.82);
-  font-size: 28px;
-  margin: 0;
-}
-
-.card {
-  height: 70vh;
-  background-color: rgba(255, 255, 255, 0.58);
-  border: none;
-}
-
-.card-image {
-  height: 70vh;
-}
-
-
-.card-title {
-  margin: 25vh 20px 10px 20px;
-  color: rgba(75, 98, 147, 0.6);
-  font-size: xx-large;
-  font-weight: bold;
-}
 </style>
