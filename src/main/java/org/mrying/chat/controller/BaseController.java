c32dcfff1ddb1239a388d9071abf6e2162ed892c
Mrying0823 <1553947217@qq.com>
2023-11-09 00:25:15 +0800

    新增用户服务、redis、JWT 工具类

--- src/main/java/org/mrying/chat/controller/BaseController.java
+++ src/main/java/org/mrying/chat/controller/BaseController.java
@@ -3,6 +3,10 @@ package org.mrying.chat.controller;
 import org.mrying.chat.service.ChatgptMessageService;
 import org.mrying.chat.service.ChatgptService;
 import org.mrying.chat.service.ConversationService;
+import org.mrying.chat.service.UserService;
+import org.mrying.chat.utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
 
@@ -15,9 +19,23 @@ public class BaseController {
     @Resource
     protected ConversationService conversationService;
 
+    // chatgpt 对话服务
     @Resource
     protected ChatgptService chatgptService;
 
+    // chatgpt 对话记录服务
     @Resource
     protected ChatgptMessageService chatgptMessageService;
+
+    // 用户服务
+    @Resource
+    protected UserService userService;
+
+    // redis
+    @Autowired
+    protected StringRedisTemplate stringRedisTemplate;
+
+    // JwtUtils
+    @Resource
+    protected JwtUtils jwtUtils;
 }
