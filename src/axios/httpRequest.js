4d78831212b99db09b441693565daea37d9651d4
Mrying0823 <1553947217@qq.com>
2023-11-09 00:52:39 +0800

    设置全局请求头，携带令牌

--- src/axios/httpRequest.js
+++ src/axios/httpRequest.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 import qs from "qs";
 
+const accessToken = localStorage.getItem("accessToken");
+
 // 设置默认值
 axios.defaults.baseURL = "http://localhost:8081/api";
 axios.defaults.timeout = 20000;
+// 设置全局请求头，携带令牌
+axios.defaults.headers.common['Authorization'] = `${accessToken}`;
 
 // get 请求方法，url 是请求的地址，params 是请求的 json 对象
 function toGet(url,params) {
