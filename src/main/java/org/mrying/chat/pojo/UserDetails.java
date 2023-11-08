f81d864555aaa382709d1148e4cffb3b361bfe6e
Mrying0823 <1553947217@qq.com>
2023-11-09 00:44:49 +0800

    实现 Spring Security 的 UserDetails 接口类，通过手机号查询的用户信息将会存储在这个对象中，包括密码，所以才能够进行隐式密码对比

---
+++ src/main/java/org/mrying/chat/pojo/UserDetails.java
@@ -0,0 +1,54 @@
+package org.mrying.chat.pojo;
+
+import org.mrying.chat.model.User;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+/**
+ * @author 邓和颖
+ * @since 2023/11/7 21:16
+ */
+public class UserDetails implements org.springframework.security.core.userdetails.UserDetails {
+
+    private User user;
+
+    public UserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getUserPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserPassword();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
