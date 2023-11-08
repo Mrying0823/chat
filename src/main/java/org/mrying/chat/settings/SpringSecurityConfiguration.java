eeeae4c0b5f877324792b4b2e54183f3a85f3947
Mrying0823 <1553947217@qq.com>
2023-11-09 00:37:01 +0800

    Spring Security 配置文件，设置无授权可访问 url，插入 JWT 过滤器，用于检验 token，放行检验通过的请求

---
+++ src/main/java/org/mrying/chat/settings/SpringSecurityConfiguration.java
@@ -0,0 +1,44 @@
+package org.mrying.chat.settings;
+
+import org.mrying.chat.filter.JwtTokenFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.annotation.Resource;
+
+/**
+ * @author 邓和颖
+ * @since 2023/11/7 15:59
+ */
+@EnableWebSecurity
+@Configuration
+public class SpringSecurityConfiguration {
+
+    @Resource
+    JwtTokenFilter jwtAuthenticationTokenFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 禁止跨域防护
+        http.csrf().disable().cors();
+
+        // 基于 token，将会话创建策略设置为无状态
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.authorizeHttpRequests()
+                .mvcMatchers("/doc.html/**","/swagger-ui.html/**", "/v2/api-docs", "/swagger-resources/**").permitAll()
+                .mvcMatchers("/v1/user/phoneExists","/v1/user/register","/v1/user/login").permitAll()
+                .anyRequest()
+                .authenticated();
+
+        // 在中间插入一个Filter
+        http.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
