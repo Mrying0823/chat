b29cd2816f7c907b053471eabf5232996cefb5f1
Mrying0823 <1553947217@qq.com>
2023-11-09 00:30:12 +0800

    新增数据库查询手机号、插入用户、查询用户数据持久

--- src/main/java/org/mrying/chat/mapper/UserMapper.java
+++ src/main/java/org/mrying/chat/mapper/UserMapper.java
@@ -1,7 +1,10 @@
 package org.mrying.chat.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.mrying.chat.model.User;
+
+import java.util.List;
 
 /**
  * @author 邓和颖
@@ -14,4 +17,13 @@ public interface UserMapper {
 
     // 扣除公用 api 使用次数
     int updateUserTokenCount(User user);
+
+    // 查询用户表中所有的手机号
+    List<String> selectUserPhone();
+
+    // 注册新用户
+    int insertNewUser(User user);
+
+    // 用户登录
+    User selectUserByPhoneAndPwd(@Param("phone") String phone, @Param("loginPassword") String passwd);
 }
