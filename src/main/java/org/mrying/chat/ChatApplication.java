439176961f7063133e9ec9bb1595622e6b8d1a49
Mrying0823 <1553947217@qq.com>
2023-11-09 00:25:56 +0800

    启动类创建 JWT 工具类对象

--- src/main/java/org/mrying/chat/ChatApplication.java
+++ src/main/java/org/mrying/chat/ChatApplication.java
@@ -1,8 +1,11 @@
 package org.mrying.chat;
 
 import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
+import org.mrying.chat.utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -13,7 +16,23 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 public class ChatApplication {
 
+    @Value("${jwt.data.secret}")
+    private String secret;
+
+    @Value("${jwt.data.expiration}")
+    private long expiration;
+
+    // 创建 JwtUtils 对象
+    @Bean
+    public JwtUtils jwtUtils() {
+        return new JwtUtils(secret, expiration);
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(ChatApplication.class, args);
+        try {
+            SpringApplication.run(ChatApplication.class, args);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
