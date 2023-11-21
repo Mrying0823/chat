9e2de6d59f3ce14d3efb7029cede7e4892070045
Mrying0823 <1553947217@qq.com>
2023-11-22 00:39:38 +0800

    quill 实现保存笔记内容；quill 设置代码块语法高亮；quill 实现 ctrl+ZYS 文本撤销、重做、保存；quill 粘贴板关闭视觉匹配，保留原始格式，解决自动添加额外行来匹配间距问题；quill 实现清除粘贴板文本格式

--- src/components/NoteQuillEditor.vue
+++ src/components/NoteQuillEditor.vue
@@ -7,7 +7,6 @@
           ref="myQuillEditor"
           :options="editorOption"
           contentType="html"
-          @update:content="setValue"
       >
       </QuillEditor>
     </el-main>
@@ -24,12 +23,123 @@
 import { QuillEditor } from '@vueup/vue-quill';
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
 // eslint-disable-next-line no-unused-vars
-import {computed, getCurrentInstance, nextTick, onMounted, ref, watch} from "vue";
+import {computed, onBeforeUnmount, onMounted, ref, watch} from "vue";
 import store from "@/store";
+// eslint-disable-next-line no-unused-vars
 import { debounce } from 'lodash';
+import {ElMessage} from "element-plus";
+import {doPost} from "@/axios/httpRequest";
+// marked 用于将 markdown 格式文本转为 html
+import * as marked from "marked";
+// highlight 用于代码高亮
+import hljs from 'highlight.js';
+import 'highlight.js/styles/monokai-sublime.css';
+import Quill from 'quill';
+import Delta from 'quill-delta';
+
+const Clipboard = Quill.import("modules/clipboard");
+
+// 阻止粘贴行为导致的滚动条跳动
+class CustomClipboard extends Clipboard {
+  onPaste(e) {
+
+    const top = window.pageYOffset;
+    const left = window.pageXOffset;
+    if (e.defaultPrevented || !this.quill.isEnabled()) return;
+    let range = this.quill.getSelection();
+    let delta = new Delta().retain(range.index);
+    this.container.style.top =
+        (
+            window.pageYOffset ||
+            document.documentElement.scrollTop ||
+            document.body.scrollTop ||
+            0
+        ).toString() + "px";
+    this.container.focus();
+    setTimeout(() => {
+      this.quill.selection.update(Quill.sources.SILENT);
+      let convertData = this.convert();
+      //循环出去文本，格式化重点
+      convertData.ops = convertData.ops.map((op) => {
+        return {
+          insert: op.insert,
+        };
+      });
+
+      delta = delta.concat(convertData).delete(range.length);
+      console.log(delta)
+      this.quill.updateContents(delta, Quill.sources.USER);
+      this.quill.setSelection(
+          delta.length() - range.length,
+          Quill.sources.SILENT
+      );
+      let bounds = this.quill.getBounds(
+          delta.length() - range.length,
+          Quill.sources.SILENT
+      );
+      this.quill.scrollingContainer.scrollTop = bounds.top;
+
+      // scroll window to previous position after paste
+      window.scrollTo({ top, left });
+    }, 1);
+
+  }
+}
+
+//此处需要注册
+Quill.register("modules/clipboard", CustomClipboard, true);
 
 const content = ref('')
 const myQuillEditor = ref()
+
+// 添加键盘事件监听器
+window.addEventListener('keydown', handleKeyDown);
+
+// 在组件销毁前移除事件监听器，以防止内存泄漏
+onBeforeUnmount(() => {
+  window.removeEventListener('keydown', handleKeyDown);
+});
+
+// 处理键盘事件
+function handleKeyDown(event) {
+  // 检查是否按下了 Ctrl 键
+  if (event.ctrlKey) {
+    // 检查是否按下了 Z 键
+    if (event.key === 'z' || event.keyCode === 90) {
+      // 在这里执行 Ctrl + Z 触发的操作
+      undoOnCtrlZ();
+    }else if(event.key === 'y' || event.keyCode === 89) {
+      // 在这里执行 Ctrl + Y 触发的操作
+      redoOnCtrlY();
+    }else if(event.key === 's' || event.keyCode === 83) {
+      // 在这里执行 Ctrl + S 触发的操作
+      saveNote();
+      // 阻止默认的保存操作，避免浏览器弹出保存框
+      event.preventDefault();
+    }
+  }
+}
+
+// 撤销
+function undoOnCtrlZ() {
+  myQuillEditor.value.getQuill().history.undo;
+}
+
+// 重做
+function redoOnCtrlY() {
+  myQuillEditor.value.getQuill().history.redo;
+}
+
+// 设置 marked 的选项及配置
+marked.setOptions({
+  // 自定义高亮代码块的渲染
+  // lang 用于指定代码块的语言
+  langPrefix: 'hljs language-',
+  highlight(code, lang) {
+    const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+    return hljs.highlight(code, { language }).value;
+  }
+});
 
 const titleConfig = [
   { Choice: '.ql-save', title: '保存' },
@@ -79,36 +189,92 @@ const titleConfig = [
   { Choice: '.ql-align .ql-picker-item[data-value="justify"]', title: '两端对齐' }
 ];
 
+//quill编辑器的字体
+// eslint-disable-next-line no-unused-vars
+const fontList = [
+  "SimSun",
+  "SimHei",
+  "Microsoft-YaHei",
+  "KaiTi",
+  "FangSong",
+  "Arial",
+  "Times-New-Roman",
+  "sans-serif",
+];
+
+/*
+var font = Quill.import("formats/font");
+font.whitelist = fontList; //将字体加入到白名单
+Quill.register(font, true);
+*/
+
+const saveNote = () => {
+
+  const text = myQuillEditor.value.getHTML();
+  const noteId = store.getters.getLastSelectedNote.noteId;
+
+  doPost("/note/updateNoteContent",{noteId: noteId, noteContent: text}).then(response => {
+    if(response && response.data.code === 200) {
+      store.commit("updateLastSelectedNote", {noteId: noteId, noteContent: text});
+      ElMessage({
+        message: '保存成功',
+        type: 'success',
+        center: true
+      });
+    }else {
+      ElMessage({
+        message: response.data.msg,
+        type: 'error',
+        center: true
+      });
+    }
+  });
+}
+
 const editorOption = {
   modules: {
-    toolbar: [
-      ['save'],
-      ['bold', 'italic', 'underline', 'strike'],
-      ['blockquote', 'code-block'],
-      [{ 'header': 1 }, { 'header': 2 }],
-      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-      [{ 'script': 'sub'}, { 'script': 'super' }],
-      [{ 'indent': '-1'}, { 'indent': '+1' }],
-      [{ 'direction': 'rtl' }],
-      [{ 'size': ['small', false, 'large', 'huge'] }],
-      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-      [{ 'color': [] }, { 'background': [] }],
-      [{ 'font': [] }],
-      [{ 'align': [] }],
-      ['clean'],
-    ]
+    toolbar: {
+      container: [
+        ['save'],
+        ['bold', 'italic', 'underline', 'strike'],
+        ['blockquote', 'code-block'],
+        [{ 'header': 1 }, { 'header': 2 }],
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],
+        [{ 'indent': '-1'}, { 'indent': '+1' }],
+        [{ 'direction': 'rtl' }],
+        [{ 'size': ['small', false, 'large', 'huge'] }],
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        [{ 'color': [] }, { 'background': [] }],
+        [{ 'font': [] }],
+        [{ 'align': [] }],
+        ['clean']
+      ],
+      handlers: {
+        save: saveNote
+      }
+    },
+    // 禁用 Quill 通过添加额外行来匹配这个间距，以弥补缺失的 margin/padding
+    clipboard: {
+      // 关闭视觉匹配，保留原始格式
+      matchVisual: false,
+    },
+    // 语法高亮
+    syntax: {
+      highlight: text => hljs.highlightAuto(text).value
+    },
+    history: {
+      delay: 2000,
+      maxStack: 500,
+      userOnly: true
+    }
   },
   placeholder: "请输入正文",
   theme: 'snow'
 };
 
-const setValue = () => {
-  const text = myQuillEditor.value.getHTML();
-  store.commit("updateLastSelectedNote",text);
-};
-
 const noteContent = computed(() => {
-  return store.state.lastSelectedNote;
+  return marked.parse(store.state.lastSelectedNote.noteContent);
 });
 
 // 防抖输入函数，用户暂停输入一段时间后 setHTML
@@ -131,7 +297,7 @@ const initButton = () => {
 
 onMounted(() => {
 
-  const noteContent = store.getters.getLastSelectedNote;
+  const noteContent = store.getters.getLastSelectedNote.noteContent;
 
   myQuillEditor.value.setHTML(noteContent);
 
