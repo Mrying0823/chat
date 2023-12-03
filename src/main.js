1744d90caf1d77b3cafb9da7abae04ef1c4b7217
Mrying0823 <1553947217@qq.com>
2023-12-03 10:25:09 +0800

    使用 pinia

--- src/main.js
+++ src/main.js
@@ -1,27 +1,29 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 import Vuex from "vuex";
-import store from './store'
+import store from './store';
 // 引入element icon
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import '@/assets/css/chatGPT.css'
-import '@/assets/css/chatGPTSidebar.css'
-import '@/assets/fonts/fontawesome-webfont.eot'
-import '@/assets/fonts/fontawesome-webfont.svg'
-import '@/assets/fonts/fontawesome-webfont.ttf'
-import '@/assets/fonts/fontawesome-webfont.woff'
-import '@/assets/css/navbar/font-awesome.min.css'
-import '@/assets/css/navbar/nav.css'
-import '@/assets/css/navbar/animations-3.css'
-import '@/assets/css/navbar/normalize.css'
-import '@/assets/css/navbar/main-versions.css'
-import '@/assets/css/floatNavbar.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+import '@/assets/css/chatGPT.css';
+import '@/assets/css/chatGPTSidebar.css';
+import '@/assets/fonts/fontawesome-webfont.eot';
+import '@/assets/fonts/fontawesome-webfont.svg';
+import '@/assets/fonts/fontawesome-webfont.ttf';
+import '@/assets/fonts/fontawesome-webfont.woff';
+import '@/assets/css/navbar/font-awesome.min.css';
+import '@/assets/css/navbar/nav.css';
+import '@/assets/css/navbar/animations-3.css';
+import '@/assets/css/navbar/normalize.css';
+import '@/assets/css/navbar/main-versions.css';
+import '@/assets/css/floatNavbar.css';
 import particlesJS from "particles.js";
-import '@/assets/css/homePage.css'
-import 'element-plus/theme-chalk/dark/css-vars.css'
+import '@/assets/css/homePage.css';
+import 'element-plus/theme-chalk/dark/css-vars.css';
+import {createPinia} from "pinia";
+
 
 // 创建应用实例
 const app = createApp(App);
@@ -30,8 +32,10 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
 
+// 使用 pinia
+app.use(createPinia());
 // 安装 Element Plus 插件
-app.use(ElementPlus)
+app.use(ElementPlus);
 // 使用 Vue Router
 app.use(router);
 // 使用 Vuex
