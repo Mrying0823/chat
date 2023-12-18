48abdbdd172d1c360a8c2cd04d4ce74ec93c3c29
Mrying0823 <1553947217@qq.com>
2023-12-19 00:46:30 +0800

    新增确认保存问题窗口

---
+++ src/components/EditQuestionConfirm.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-dialog v-model="storeQuestion.confirmDialog" title="Shipping address" :destroy-on-close="true">
+    <template #footer>
+      <span>
+        <el-button @click="storeQuestion.confirmDialog = false">Cancel</el-button>
+        <el-button type="primary" @click="storeQuestion.confirmDialog = false">
+          Confirm
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<script setup>
+import {useQuestionData} from "@/store/questionData";
+
+// eslint-disable-next-line no-unused-vars
+const storeQuestion = useQuestionData();
+</script>
+
+<style scoped>
+
+</style>
