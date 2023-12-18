9edd7fdf1b6a0985c21e3759616ce14117770f41
Mrying0823 <1553947217@qq.com>
2023-12-19 00:45:38 +0800

    完善用户问题列表加载功能逻辑

--- src/views/MyQuestionListView.vue
+++ src/views/MyQuestionListView.vue
@@ -1,6 +1,6 @@
 <template>
   <NavBar/>
-<!--  <LoadingBar v-show="loadingDisplay.display"></LoadingBar>-->
+  <LoadingBar v-show="loadingDisplay.display"></LoadingBar>
   <el-container :class="this.$store.getters.getDarkMode ? 'night-mode-chat-container': 'chat-container'" style="overflow: hidden">
     <el-header>
       <SearchBar
@@ -17,6 +17,7 @@
       <QuestionList
           :table-data="tableData"
           :total-rows="totalRows"
+          :edit="true"
       />
     </el-main>
   </el-container>
@@ -26,9 +27,75 @@
 import NavBar from "@/components/NavBar";
 import QuestionList from "@/components/QuestionList";
 import SearchBar from "@/components/SearchBar";
-// import LoadingBar from "@/components/LoadingBar"
+import LoadingBar from "@/components/LoadingBar"
 import QuestionDetails from "@/components/QuestionDetails";
+import {doPost, toGet} from "@/axios/httpRequest";
+import {ElMessage} from "element-plus";
+import {useLoadingDisplay} from "@/store/loadingDisplay";
+import {useQuestionData} from "@/store/questionData";
+import {onMounted, ref, watchEffect} from "vue";
 
+const loadingDisplay = useLoadingDisplay();
+
+const storeQuestion = useQuestionData();
+
+let tableData = ref([]);
+let totalRows = ref(0);
+
+function doInitPost() {
+  toGet("/questionList/forPublicQuestionList").then(response => {
+    if(response && response.data.code === 200) {
+      storeQuestion.subjectList = response.data.map.subjectList;
+      storeQuestion.difficultyList = response.data.map.difficultyList;
+      storeQuestion.stageList = response.data.map.stageList;
+    }else {
+      ElMessage({
+        type: "warning",
+        center: true,
+        message: response.data.msg
+      });
+    }
+  });
+}
+
+function doPageChangePost() {
+  doPost("/questionList/queryUserQuestionList",{pageNo: storeQuestion.pageNo,
+                                                        pageSize: storeQuestion.pageSize,
+                                                        subject: storeQuestion.subject,
+                                                        keyword: storeQuestion.keyword,
+                                                        difficulty: storeQuestion.difficulty,
+                                                        createdAt: storeQuestion.createdDate,
+                                                        updatedAt: storeQuestion.updatedDate
+  }).then(response => {
+    if(response && response.data.code === 200) {
+      tableData.value = response.data.map.questionList;
+      totalRows.value = response.data.map.totalRows;
+      setTimeout(() => {
+        loadingDisplay.display = false;
+      },1000);
+    }else {
+      ElMessage({
+        type: "warning",
+        center: true,
+        message: response.data.msg
+      });
+    }
+  });
+}
+
+onMounted(() => {
+  loadingDisplay.display = true;
+
+  doPageChangePost();
+
+  watchEffect(() => {
+    if(storeQuestion) {
+      doPageChangePost();
+    }
+  });
+
+  doInitPost();
+});
 </script>
 
 <style scoped>
